3f6de6ac7f40766bf6147bf8178ee3dcffc4532f
You Wu <3085031@sina.com>
2017-11-09 19:37:33 +0800

    Added QC tests for string and number.
    Added QC tests for string and number.

--- APle/SyntaxQC.hs
+++ APle/SyntaxQC.hs
@@ -4,7 +4,42 @@ import Syntax
 import Test.QuickCheck
 
 -- Your code here
+opt :: OpTable
+opt = OpTable [(FNone, ["<=", "<"]), (FLeft, ["+", "-"]), (FLeft, ["*"]), (FRight, ["**"])]
+
+characters :: String
+characters = ['a'..'z'] ++ ['A'..'Z']
+
+numbers :: String
+numbers = ['0'..'9']
+
+-- A quick test to test parse number
+-- if n < 0, add "~" before the absolute value of n
+prop_int :: Integer -> Bool
+prop_int n = if n >= 0
+    then (parseStringTerm opt (show n)) == (Right (TNum n))
+    else (parseStringTerm opt ("~" ++ (show (abs n)))) == (Right (TNum n))
+
+-- A quick check test to test parse string
+genSyntaxString :: Gen String
+genSyntaxString = listOf1 $ elements $ characters ++ numbers
+
+genChar :: Gen String
+genChar = resize 1 $ listOf1 $ elements characters
+
+newtype SyntaxString = SyntaxString { genString :: String }
+    deriving Show
+
+instance Arbitrary SyntaxString where
+    arbitrary = do
+        c <- genChar
+        cs <- genSyntaxString
+        return $ SyntaxString (c ++ cs)
+
+prop_string :: SyntaxString -> Bool
+prop_string s = let str = (genString s) in
+    (parseStringTerm opt str) == (Right $ TVar str)
 
 -- main test function, to make tests runnable as "runhaskell SyntaxQC"
 main :: IO ()
-main = undefined
+main = quickCheck prop_int >> quickCheck prop_string
